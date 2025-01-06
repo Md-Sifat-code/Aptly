3e094e875e5f3403815fd536fdb16c41a20e5b2c
Md-Sifat-code <codewithsifat4@gmail.com>
2025-01-06 23:03:58 +0600

    added

--- src/Modals/ProfileDrawer.jsx
+++ src/Modals/ProfileDrawer.jsx
@@ -7,7 +7,7 @@ const ProfileDrawer = ({ closeDrawer }) => {
 
   return (
     <div className="pop fixed inset-0 z-50 bg-gray-800 bg-opacity-50 flex justify-end">
-      <div className="bg-white w-72 h-auto max-h-[90vh] shadow-lg p-6 rounded-bl-lg rounded-br-lg rounded-tl-lg rounded-tr-[42px] flex flex-col justify-center mt-12 mr-4">
+      <div className="bg-white px-4 w-72 h-auto max-h-[90vh] shadow-lg  rounded-bl-lg rounded-br-lg rounded-tl-lg rounded-tr-[42px] flex flex-col justify-center mt-12 mr-4">
         {/* Close button with icon */}
         <button
           onClick={closeDrawer}
@@ -24,23 +24,28 @@ const ProfileDrawer = ({ closeDrawer }) => {
             className="w-16 h-16 rounded-full mr-4"
           />
           <div>
-            <h2 className="text-2xl">{user?.username || "User Name"}</h2>
-            <p className="text-sm text-gray-500">
-              {user?.location || "Location"}
+            <h2 className="text-2xl text-center font-bold">
+              {user?.username || "User Name"}
+            </h2>
+            <p className="text-sm text-center text-gray-500">
+              {user?.location || "Dhaka, Bangladesh"}
             </p>
-            <p className="text-sm text-gray-500">
-              {user?.contact || "Contact Info"}
+            <p className="text-sm text-center text-gray-500">
+              {user?.contact || "017xxxxxxxxxx"}
             </p>
-            <p className="text-sm text-gray-500">{user?.gmail || "Email"}</p>
+            <p className="text-sm text-gray-500 text-center">
+              {user?.gmail || "user@gmail.com"}
+            </p>
           </div>
         </div>
 
         <div className="mt-6">
-          <div className="flex items-center">
+          <div className="flex flex-col  p-4 text-teal-700 border-2  border-teal-700 items-center">
             {/* Display sold properties count */}
-            <FaEnvelope size={20} className="mr-2" />
-            <p className="text-sm text-gray-600">
-              Properties sold: {user?.propertiesSold || 0}
+            <FaHome size={25} className="mr-2" />
+            <p className="text-sm flex flex-col text-center mt-4 text-black">
+              {user?.propertiesSold || 0}
+              <span>Sold/Rent</span>
             </p>
           </div>
         </div>

9c75160ca51f4ec852a2c23d72b43953b0177a48
Md-Sifat-code <codewithsifat4@gmail.com>
2025-01-09 01:40:01 +0600

    fixed the footer

--- src/Fixed_Layout/Footer.jsx
+++ src/Fixed_Layout/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import home from "/bariss.png";
+import home from "/okay.png";
 export default function Footer() {
   return (
-    <footer className="bg-[#84b5b9] border-t mt-12 py-10">
+    <footer className="bg-[#006d6f] border-t mt-12 py-10">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-start gap-8">
           {/* Company Info */}
           <aside className="text-center md:text-left">
             <div className="flex items-center justify-center md:justify-start mb-4">
-              <img className="w-[50px]" src={home} alt="" />
+              <img className="w-[120px]" src={home} alt="" />
             </div>
-            <p className="text-gray-700">
+            <p className="text-white">
               <strong>Bari Bazar BD</strong>
               <br />
               Providing reliable Home since 2024
@@ -19,25 +19,25 @@ export default function Footer() {
 
           {/* Services */}
           <nav>
-            <h6 className="text-lg font-bold mb-4 text-gray-800">Services</h6>
+            <h6 className="text-lg font-bold mb-4 text-black">Services</h6>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Branding
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Design
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Marketing
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Advertisement
                 </a>
               </li>
@@ -46,25 +46,25 @@ export default function Footer() {
 
           {/* Company */}
           <nav>
-            <h6 className="text-lg font-bold mb-4 text-gray-800">Company</h6>
+            <h6 className="text-lg font-bold mb-4 text-black">Company</h6>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   About us
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Contact
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Jobs
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Press kit
                 </a>
               </li>
@@ -73,20 +73,20 @@ export default function Footer() {
 
           {/* Legal */}
           <nav>
-            <h6 className="text-lg font-bold mb-4 text-gray-800">Legal</h6>
+            <h6 className="text-lg font-bold mb-4 text-black">Legal</h6>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Terms of use
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Privacy policy
                 </a>
               </li>
               <li>
-                <a href="#" className="link link-hover text-gray-600">
+                <a href="#" className="link link-hover text-white">
                   Cookie policy
                 </a>
               </li>

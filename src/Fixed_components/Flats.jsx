d0fe4e7985f5a87a4d19de489df741c09e670169
Md-Sifat-code <codewithsifat4@gmail.com>
2025-01-12 00:51:42 +0600

    added the user part into the flats

--- src/Fixed_components/Flats.jsx
+++ src/Fixed_components/Flats.jsx
@@ -33,7 +33,10 @@ const Flats = () => {
             <p className="text-sm flex items-center gap-2 font-medium text-start  w-full">
               {flat.location}
             </p>
-            <p className="text-sm flex items-center text-gray-500 gap-2 font-normal text-start mb-2 mt-1 w-full">
+            <p className="text-sm flex items-center gap-2 font-normal text-start text-gray-400 w-full">
+              Hosted by {flat.user.fullname}
+            </p>
+            <p className="text-sm flex items-center text-gray-500 gap-2 font-medium text-start mb-2 mt-1 w-full">
               {flat.price} BDT
             </p>
           </Link>

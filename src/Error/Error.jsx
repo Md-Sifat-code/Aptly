bcd6ece84e04fc7209c3da86097ac57601df0b5a
Md-Sifat-code <codewithsifat4@gmail.com>
2025-02-05 23:34:44 +0600

    some error fixed into the error page

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -16,15 +16,15 @@ function Error() {
         <div className="text-center">
           <FaExclamationTriangle className="mx-auto h-12 w-12 text-red-500" />
           <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-            Something Went Wrong
+            Under Development
           </h2>
           <p className="mt-2 text-sm text-gray-600">
-            We encountered an unexpected error. Please try again later or
-            contact support if the issue persists.
+            We building up this page for better exprience Thanks for your
+            support. Keep Brousing!.
           </p>
         </div>
 
-        <div className="mt-6 flex justify-center">
+        <div className=" flex justify-center">
           <button
             onClick={handleBackClick} // Call handleBackClick when the button is clicked
             className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
